a6da46c2260373849f474074bdba70da1eb9a7e7
miller <957776740@qq.com>
2022-09-24 23:33:43 +0800

    HW1参数调整；

--- HW01/HW1/wangshuo_hw1.py
+++ HW01/HW1/wangshuo_hw1.py
@@ -21,6 +21,10 @@ from torch.utils.data import Dataset, DataLoader, random_split
 # For plotting learning curve
 from torch.utils.tensorboard import SummaryWriter
 
+    # https://drive.google.com/uc?export=download&id=1kLSW_-cW2Huj7bh84YTdimGBOJaODiOS
+    # https://drive.google.com/uc?export=download&id=1iiI5qROrAhZn-o4FPqsE97bMzDEFvIdg
+# !gdown --id '1kLSW_-cW2Huj7bh84YTdimGBOJaODiOS' --output covid.train.csv
+# !gdown --id '1iiI5qROrAhZn-o4FPqsE97bMzDEFvIdg' --output covid.test.csv
 
 def same_seed(seed):
     '''Fixes random number generator seeds for reproducibility.'''
@@ -83,16 +87,9 @@ class My_Model(nn.Module):
         self.layers = nn.Sequential(
             nn.Linear(input_dim, 64),
             nn.ReLU(),
-            nn.Linear(64, 8),
+            nn.Linear(64, 16),
             nn.ReLU(),
-            nn.Linear(8, 4),
-            nn.ReLU(),
-            nn.Linear(4, 1)
-            # nn.Linear(input_dim, 16),
-            # nn.ReLU(),
-            # nn.Linear(16, 8),
-            # nn.ReLU(),
-            # nn.Linear(8, 1)
+            nn.Linear(16, 1)
         )
 
     def forward(self, x):
@@ -109,7 +106,10 @@ def select_feat(train_data, valid_data, test_data, select_all=True):
     if select_all:
         feat_idx = list(range(raw_x_train.shape[1]))
     else:
-        feat_idx = [0, 1, 2, 3, 4]  # TODO: Select suitable feature columns.
+        # feat_idx = [0, 1, 2, 3, 4]  # TODO: Select suitable feature columns.
+        # 相关性大于0.8的特征
+        feat_idx = list(range(1, 38)) + [38, 39, 40, 41, 53, 54, 55, 56, 57, 69, 70, 71, 72, 73, 85, 86, 87, 88, 89, 101, 102, 103, 104, 105]
+        # feat_idx = list(range(1, 38)) + [53, 69, 85, 101]
 
     return raw_x_train[:, feat_idx], raw_x_valid[:, feat_idx], raw_x_test[:, feat_idx], y_train, y_valid
 
@@ -120,7 +120,9 @@ def trainer(train_loader, valid_loader, model, config, device):
     # Define your optimization algorithm.
     # TODO: Please check https://pytorch.org/docs/stable/optim.html to get more available algorithms.
     # TODO: L2 regularization (optimizer(weight decay...) or implement by your self).
-    optimizer = torch.optim.SGD(model.parameters(), lr=config['learning_rate'], momentum=0.9)
+    # optimizer = torch.optim.SGD(model.parameters(), lr=config['learning_rate'], momentum=0.9)
+    # optimizer = torch.optim.AdamW(model.parameters(), lr=config['learning_rate'], weight_decay=0.08)
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['learning_rate'] * 100, weight_decay=1e-3)
 
     writer = SummaryWriter()  # Writer of tensoboard.
 
@@ -193,11 +195,11 @@ def save_pred(preds, file):
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     config = {
-        'seed': 5201314,  # 制定模型的随即种子以保证模型的可恢复性。Your seed number, you can pick your lucky number. :)
-        'select_all': True,  # Whether to use all features.
+        'seed': 617,  # 制定模型的随即种子以保证模型的可恢复性。Your seed number, you can pick your lucky number. :)
+        'select_all': False,  # Whether to use all features.
         'valid_ratio': 0.2,  # validation_size = train_size * valid_ratio
         'n_epochs': 3000,  # Number of epochs.
-        'batch_size': 128,  #256,
+        'batch_size': 128,
         'learning_rate': 1e-5,
         'early_stop': 400,
         # If model has not improved for this many consecutive epochs, stop training. 任一时刻连续400次没有模型训练降低loss，就会提前停止。
@@ -209,7 +211,7 @@ if __name__ == '__main__':
 
     # train_data size: 2699 x 118 (id + 37 states + 16 features x 5 days)
     # test_data size: 1078 x 117 (without last day's positive rate)
-    train_data, test_data = pd.read_csv('./covid.train.csv').values, pd.read_csv('./covid.test.csv').values
+    train_data, test_data = pd.read_csv('./covid.train_new.csv').values, pd.read_csv('./covid.test_un.csv').values
     train_data, valid_data = train_valid_split(train_data, config['valid_ratio'], config['seed'])  # 按照k折交叉验证法分成训练集和验证集
 
     # Print out the data size.
