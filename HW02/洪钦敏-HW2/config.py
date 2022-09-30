a0a0f1987adbe89858d468f4a9afb5e8912e5fc1
kk <akai_sola@qq.com>
2022-09-30 12:40:21 +0800

    HW2 跑了5个机车

--- HW02/洪钦敏-HW2/config.py
+++ HW02/洪钦敏-HW2/config.py
@@ -1,5 +1,5 @@
 # data prarameters
-concat_nframes = 7              # the number of frames to concat with, n must be odd (total 2k+1 = n frames)
+concat_nframes = 11              # the number of frames to concat with, n must be odd (total 2k+1 = n frames)
 train_ratio = 0.9               # the ratio of data used for training, the rest will be used for validation
 # training parameters
 seed = 0                        # random seed
@@ -9,8 +9,8 @@ learning_rate = 1e-3          # learning rate
 # model parameters
 input_dim = 39 * concat_nframes # the input dim of the model, you should not change the value
 hidden_layers = 3               # the number of hidden layers
-hidden_dim = 256                # the hidden dim
-version = 6
+hidden_dim = 512               # the hidden dim
+version = 9
 weight_decay = 1e-2           # weight decay
 model_path = './modelv%d.ckpt'%version     # the path where the checkpoint will be saved
 save_path = 'predictionv%d.csv'%version
